7054eceb33cfa0925f47aa793b4b05ab2bd58733
Nalam Manasa Venkata Naga Sai <nalammanasa2002@gmail.com>
2023-03-06 17:45:15 +0530

    adds inputComponent

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <input
+    type="text"
+    class="rounded-md border-1 bg-stone-500 border-slate-400 m-1 px-2 py-1"
+  >
+  <input type="text" v-model="second" class="bg-red-600 p-2">
+  {{ second }}
+</template>
+
+<script setup>
+import { ref , watch } from 'vue';
+
+const second = ref("")
+//const text = ref(props.modelValue);
+
+const props = defineProps ({
+  modelValue: {
+    type: String,
+    default:''
+  }
+})
+const emits = defineEmits(["update:modelValue"]);
+
+console.log(props.modelValue);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    
+    emits("update:modelValue", "changed");
+  }
+  
+);
+
+const string = props.modelValue;
+
+
+for (let index = 0; index < string.length; index++) {
+  //newString =newString + String.fromCharCode(string.charCodeAt(index) + 1);
+}
+
+
+watch(
+  second,
+  (value) => {
+    console.log("here")
+    const string = value;
+    let newString = '';
+    for (let index = 0; index < string.length; index++) {
+      newString = newString + String.fromCharCode(string.charCodeAt(index) + 1);
+    }
+    console.log( newString )
+    second.value = newString
+    
+  }
+)
+
+
+/*
+  () => props.modelValue,
+  (value) => { 
+    const letters = [...text];
+    console.log(letters)
+    //emits("update:modelValue", incrementedText);
+    emits("update:modelValue", value+"added");
+  }
+*/
+
+//String.fromCharCode('A'.charCodeAt() + 1) // Returns B
+</script>
+
+
