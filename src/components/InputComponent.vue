8fb10169627da246856c39b839ad14d0c6de6e79
Nalam Manasa Venkata Naga Sai <nalammanasa2002@gmail.com>
2023-03-07 12:03:17 +0530

    changes InputComponent

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,55 +1,47 @@
 <template>
-  <input
-    type="text"
-    v-model="localValue"
-    class="rounded-md outline-slate-600 bg-slate-200 m-1 px-2 py-1"
-  >
-
+  <input v-model="localValue" class="bg-white border border-black h-fit rounded-md px-3 py-1" @keyup="onInput">
 </template>
 
 <script setup>
-import { ref , watch } from 'vue';
+import { onMounted, ref } from 'vue'
+import { watch } from "vue"
 
-const props = defineProps ({
+const props = defineProps({
   modelValue: {
     type: String,
-    default:'',
+    default: ''
   }
 })
 
-const emits = defineEmits(["update:modelValue"]);
-const localValue = ref(props.modelValue);
-const length = ref(0);
-let count = 0;
+const emits = defineEmits(['update:modelValue'])
 
-const jump_ascii = function(value){
-  if(count === 1){
-    const string = value;
-    let newString = '';
-    for (let index = 0; index < string.length; index++) {
-      newString = newString + String.fromCharCode(string.charCodeAt(index) + 1);
-    }
-    return newString;
-  }
+const incrAscii = function (v) {
+  let code = v.charCodeAt(0)
+  return String.fromCharCode(++code)
 }
 
-const saveLength = function(len){
-  length.value = len;
+const modify = function (str) {
+  return str.split('').map(x => incrAscii(x)).join('')
 }
 
-watch(
-  () => props.modelValue,
-  (value) => {
-    if(length.value !== value.length ){
-      count = 1;
-      localValue.value = jump_ascii(value);
-      saveLength(value.length);
-    }
-  }
-);
+const localValue = ref('')
+
+watch(() => props.modelValue, (value) => {
+  localValue.value = value
+})
 
 watch(localValue, (value) => {
-  emits("update:modelValue", value);
+  emits('update:modelValue', value)
+})
+
+const onInput = function (e) {
+  if (['Control', 'Alt', 'Shift', 'Meta', 'Backspace'].includes(e.key)) {
+    return
+  }
+  emits('update:modelValue', localValue.value.slice(0, -1) + modify(e.key))
 }
-)
+
+onMounted(() => {
+  localValue.value = modify(props.modelValue)
+})
 </script>
